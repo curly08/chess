1983f6ad5ecfd3ffbaf4ee8d7f844856f30606e0
curly08 <mattynoakes08@gmail.com>
2021-11-02 15:13:10 -0400

    Add #make_spaces

--- lib/board.rb
+++ lib/board.rb
@@ -4,15 +4,24 @@ require_relative '../lib/space'
 
 # board display
 class Board
-  attr_accessor :spaces
-  attr_reader :row_separator, :rank_row, :ranks, :files
+  attr_reader :row_separator, :rank_row, :ranks, :files, :spaces
 
   def initialize
     @row_separator = '  +---+---+---+---+---+---+---+---+'
     @rank_row = '    a   b   c   d   e   f   g   h'
     @ranks = %w[a b c d e f g h]
     @files = %w[8 7 6 5 4 3 2 1]
-    @spaces = []
+    @spaces = make_spaces
+  end
+
+  def make_spaces
+    arr = []
+    files.each do |file|
+      ranks.each do |rank|
+        arr << Space.new(rank, file)
+      end
+    end
+    arr
   end
 
   def make_grid
@@ -20,8 +29,8 @@ class Board
     files.each do |file|
       print "#{file} |"
       ranks.each do |rank|
-        spaces << Space.new(rank, file)
-        print " #{spaces.last.data} |"
+        square = spaces.select { |space| space.location == [rank, file].join }.pop
+        print " #{square.data} |"
       end
       puts "\n#{row_separator}"
     end
