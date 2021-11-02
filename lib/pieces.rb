53d0d0a084de88ac7ee50be67f90f13243c7d083
curly08 <mattynoakes08@gmail.com>
2021-11-02 15:14:55 -0400

    Add attr_reader for Pawn class instance variables

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# game pieces
+# Pawn class
 class Pawn
   attr_accessor :location
-  attr_reader :color
+  attr_reader :color, :symbol
 
   @white_starting_locations = %w[a2 b2 c2 d2 e2 f2 g2 h2]
   @black_starting_locations = %w[a7 b7 c7 d7 e7 f7 g7 h7]
@@ -15,7 +15,7 @@ class Pawn
     @legal_moves = []
   end
 
-  # def get_legal_moves
-
-  # end
+  class << self
+    attr_reader :white_starting_locations, :black_starting_locations
+  end
 end
