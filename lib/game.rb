324395e5268c539504eb8dc91a48ab5748cd262f
curly08 <mattynoakes08@gmail.com>
2021-11-03 16:22:35 -0400

    Add #play_move

--- lib/game.rb
+++ lib/game.rb
@@ -25,12 +25,13 @@ class Game
       pieces.each { |piece_type| generate_pieces(player, piece_type) }
     end
     show_board
-    # loop do
-    #   players[0].play_move
-    #   return game_over_message if game_over == true
+    loop do
+      play_move(players[0])
+      show_board
+      # return game_over_message if game_over == true
 
-    #   players.rotate!
-    # end
+      players.rotate!
+    end
   end
 
   def establish_players
@@ -51,15 +52,9 @@ class Game
     starting_locations = player.color == 'white' ? piece_class.white_starting_locations : piece_class.black_starting_locations
     starting_locations.each do |location|
       new_piece = piece_class.new(player.color, location)
-      populate_square(new_piece, location)
+      board.populate_square(new_piece, location)
       player.pieces << new_piece
     end
-  end
-
-  def populate_square(new_piece, piece_location)
-    selected_square = board.squares.select { |square| square.location == piece_location }.pop
-    selected_square.piece = new_piece
-    selected_square.data = new_piece.symbol
   end
 
   def show_board
@@ -67,8 +62,11 @@ class Game
     puts "#{players[0].name.upcase}(#{players[0].color})    V    #{players[1].name.upcase}(#{players[1].color})\n"
     board.make_grid
   end
-end
 
-game = Game.new
-# binding.pry
-game.play_game
+  def play_move(player)
+    selected_piece = player.select_piece
+    selected_move = player.select_move(selected_piece)
+    board.clear_square(selected_piece.location)
+    board.populate_square(selected_piece, selected_move)
+  end
+end
