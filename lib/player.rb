fe96a9dba53bf08505eccc8124e2b439620544ed
curly08 <mattynoakes08@gmail.com>
2021-11-03 16:10:05 -0400

    Add #select_piece and #select_move

--- lib/player.rb
+++ lib/player.rb
@@ -10,4 +10,24 @@ class Player
     @color = nil
     @pieces = []
   end
+
+  def select_piece
+    puts "#{name}, select a piece to move. Ex. \"#{pieces.sample.location}\""
+    loop do
+      input = gets.chomp
+      return pieces.select { |piece| piece.location == input }.pop if pieces.any? { |piece| piece.location == input }
+
+      puts "#{input} is not a valid selection."
+    end
+  end
+
+  def select_move(piece)
+    puts "#{name}, where would you like to move #{piece.location}? Ex. \"#{piece.legal_moves.sample}\""
+    loop do
+      input = gets.chomp
+      return input if piece.legal_moves.include?(input)
+
+      puts "#{input} is not a valid move."
+    end
+  end
 end
